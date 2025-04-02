3341609423534b83e03cced4011f0a4495469fbd
Sunil-Indoria-CT <sunil.i@cloudthat.com>
2025-04-02 16:40:40 +0530

    Ignored .exe & .vscode files

---
+++ 01_Learn_the basics/01_Things-to-know-in-CPP/02-Data-Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int data = 12;
+    float f = 11.5;
+    string name = "Coding";
+    char c = 'a';
+    cout << data<<endl;
+    cout << f<<endl;
+    cout << name<<endl;
+    cout << c<<endl;
+    return 0;
+}
