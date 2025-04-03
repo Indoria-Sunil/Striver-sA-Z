417ff528653ba51eecf0f9f4b4cd29bc9d80b407
Sunil-Indoria-CT <sunil.i@cloudthat.com>
2025-04-03 15:44:15 +0530

    Added

--- 01_Learn_the basics/02-BuildUp-Logical-Thinking/01-Patterns/07-Pattern.cpp
+++ 01_Learn_the basics/02-BuildUp-Logical-Thinking/01-Patterns/07-Pattern.cpp
@@ -7,14 +7,14 @@ void printingPatter(int num)
 
     for (int i = 0; i < num; i++)
     {
-        for (int k = 0; k < num-i-1; k++)
+        for (int k = 0; k < num - i - 1; k++)
         {
-            cout<<" ";
+            cout << " ";
         }
-        
-        for (int j = 0; j < 2*i + 1; j++)
+
+        for (int j = 0; j < 2 * i + 1; j++)
         {
-            cout  << "*";
+            cout << "*";
         }
         cout << endl;
     }
