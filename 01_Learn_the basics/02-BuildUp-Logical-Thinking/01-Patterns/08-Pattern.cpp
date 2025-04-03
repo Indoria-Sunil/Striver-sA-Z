262d473569443d9113a7e1c8321115bf87b64af6
Sunil-Indoria-CT <sunil.i@cloudthat.com>
2025-04-03 15:55:22 +0530

    Added pattern

---
+++ 01_Learn_the basics/02-BuildUp-Logical-Thinking/01-Patterns/08-Pattern.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printingPatter(int num)
+{
+
+    for (int i = num; i > 0; i--)
+    {
+        for (int k = 0; k < num - i + 1; k++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    printingPatter(num);
+    return 0;
+}
