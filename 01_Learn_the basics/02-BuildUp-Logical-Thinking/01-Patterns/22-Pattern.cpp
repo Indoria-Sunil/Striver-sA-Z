75ec12bad4f36835d1a6284d60fc6be7312213cd
Indoria-Sunil <sunilindoriacse@gmail.com>
2025-06-19 21:24:56 +0530

    Modified

--- 01_Learn_the basics/02-BuildUp-Logical-Thinking/01-Patterns/22-Pattern.cpp
+++ 01_Learn_the basics/02-BuildUp-Logical-Thinking/01-Patterns/22-Pattern.cpp
@@ -2,13 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printingPattern(int num)
+void printingPattern(int n)
 {
-    for (int i = 0; i < num * 2 - 1; i++)
+    for (int i = 0; i < n * 2 - 1; i++)
     {
-        for (int j = 0; j < num; j++)
+        for (int j = 0; j < n * 2 - 1; j++)
         {
-            
+            int top = i;
+            int bottom = 2 * n - 1 - i;
+            int right = 2 * n - 2 - j;
+            int left = j;
+            int mini = min(top, min(bottom, min(left, right)));
+            cout<<n - mini<<" ";
         }
         cout << endl;
     }
