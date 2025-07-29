7eeb318ad2a983e656cbaed6166fbebab00bf65b
Indoria-Sunil <sunilindoriacse@gmail.com>
2025-07-29 15:54:25 +0530

    Checking for Sorted Array

---
+++ 03-Arrays/02-Medium/03-Check-Sorted-Array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSorted(vector<int> nums)
+{
+    if (nums.size() == 0 || nums.size() == 1)
+        return true;
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i] < nums[i - 1])
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5, 0};
+    if (isSorted(nums))
+        cout << "Array is sorted";
+    else
+        cout << "Array is not sorted";
+
+    return 0;
+}
