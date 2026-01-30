c3ce758ea7dbeda436d344a70a5e171b6d0a8775
Sunil_indoria <sunilindoriacse@gmail.com>
2026-01-30 13:15:56 +0530

    Added finding square root of a number

---
+++ 04-Binary-Search/02-BS-On-Answers/01-Find-Square-Root-Of-A-Number.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findingSquareRoot(int number)
+    {
+        if (number == 1)
+            return 1;
+        int low = 1, high = number;
+        int ans;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (mid * mid <= number)
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+            else
+                high = mid - 1;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    int number;
+    cout << "Enter the number: ";
+    cin >> number;
+    Solution obj;
+    cout << "The sqaure root of the given number is: " << obj.findingSquareRoot(number);
+
+    return 0;
+}
